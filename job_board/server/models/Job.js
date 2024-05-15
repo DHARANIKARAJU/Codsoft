3f0f6b879f5a23b52ce26b9af9c312cd85541040
DHARANIKA R <124855076+DHARANIKARAJU@users.noreply.github.com>
2024-05-15 21:12:45 +0530

    Add files via upload

---
+++ job_board/server/models/Job.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+
+const jobSchema = new mongoose.Schema({
+  title: String,
+  description: String,
+  company: String,
+  location: String,
+  salary: String
+});
+
+const Job = mongoose.model('Job', jobSchema);
+
+module.exports = Job;
